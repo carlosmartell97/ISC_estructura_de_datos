87d93dc23bd17ccbd403963d0d79d159e1d56a6f
carlosmartell97 <carlosm_97@live.com>
2016-09-01 18:08:36 -0500

    intento changed

--- 1 parcial/Midterm _Practice/src/intento.java
+++ 1 parcial/Midterm _Practice/src/intento.java
@@ -1,4 +1,16 @@
 
 public class intento<Item> extends ArrayLinearList<Item>{
+	private int size;
+	private Item[] element;
+	private int maxSize=100;
+	public intento(){
+		this.size=0;
+		this.element=(Item[]) new Object[maxSize];
+	}
 	
+	public static void main(String[] args) {
+		intento<Integer> lista = new intento<Integer>();
+		lista.add(0, 1);
+		System.out.println(lista);
+	}
 }
