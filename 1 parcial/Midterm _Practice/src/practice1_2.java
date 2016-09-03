4c0a8f7ccd077784a91cb11e8c260483edcae289
carlosmartell97 <carlosm_97@live.com>
2016-09-02 23:05:37 -0500

    basic structure

--- 1 parcial/Midterm _Practice/src/practice1_2.java
+++ 1 parcial/Midterm _Practice/src/practice1_2.java
@@ -1,4 +1,9 @@
-
-public class practice1_2 {
-
+public class practice1_2<T>{
+	private ChainNode<T> firstNode;
+	private int size;
+	
+	private static class ChainNode<T>{
+		T element;
+		ChainNode<T> next;
+	}
 }
