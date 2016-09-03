e0ea7e30ad875a878f745b94d4ef07d30c10fb88
carlosmartell97 <carlosm_97@live.com>
2016-09-02 22:51:10 -0500

    here it works with Integers

--- 1 parcial/Midterm _Practice/src/practice1_1.java
+++ 1 parcial/Midterm _Practice/src/practice1_1.java
@@ -5,16 +5,17 @@ public class practice1_1{
 	
 	public void rightShift(int k){
 		int[] newElement = new int[this.element.length+k];
-		for(int i=0;i<k;i++){
-			this.element[i]=0;
-		}
-		System.arraycopy(this.element, k, newElement, k, newElement.length);
+		/*for(int i=0;i<k;i++){
+			newElement[i]=0;
+		}*/
+		System.arraycopy(this.element, 0, newElement, k, this.element.length);
+		this.element=newElement;
 	}
 	
 	public static void main(String[] args) {
 		practice1_1 o = new practice1_1();
 		System.out.println(Arrays.toString(o.element));
-		int[] d = {3,2,1};
-		System.out.println(d.toString());
+		o.rightShift(2);
+		System.out.println(Arrays.toString(o.element));
 	}
 }
