5343372b7a837c418e485a38fbbcfcf4f1ce209c
carlosmartell97 <carlosm_97@live.com>
2016-09-02 22:30:12 -0500

    making progress, finally

--- 1 parcial/Midterm _Practice/src/practice1_1.java
+++ 1 parcial/Midterm _Practice/src/practice1_1.java
@@ -1,3 +1,11 @@
-public class practice1_1<Item>{
+public class practice1_1<T>{
+	private T[] element;
 	
+	public void rightShift(int k){
+		T[] newElement = (T[]) new Object[this.element.length+k];
+		for(int i=0;i<k;i++){
+			this.element[i]=0;
+		}
+		System.arraycopy(this.element, k, newElement, k, newElement.length);
+	}
 }
