856c3350a89ef81f8920f2ea9e1cf7318a008eb4
carlosmartell97 <carlosm_97@live.com>
2016-09-03 17:40:31 -0500

    y aquí ya le puse genéricos

--- 1 parcial/Midterm _Practice/src/practice2_1.java
+++ 1 parcial/Midterm _Practice/src/practice2_1.java
@@ -1,11 +1,11 @@
 //ya funciona con Integers.
 import java.util.Arrays;
 
-public class practice2_1 {
-	private int[] element={1,2,3,4,5,6,7};
+public class practice2_1<T>{
+	private T[] element;
 	
 	public void compress(){
-		int[] newElement=new int[(1+this.element.length)/2];
+		T[] newElement=(T[]) new Object[(1+this.element.length)/2];
 		for(int i=0,x=0;i<this.element.length;i=i+2,x=x+1){
 			newElement[x]=this.element[i];
 		}
@@ -13,8 +13,8 @@ public class practice2_1 {
 	}
 	
 	public static void main(String[] args) {
-		practice2_1 o = new practice2_1();
+		/*practice2_1 o = new practice2_1();
 		o.compress();
-		System.out.println(Arrays.toString(o.element));
+		System.out.println(Arrays.toString(o.element));*/
 	}
 }
