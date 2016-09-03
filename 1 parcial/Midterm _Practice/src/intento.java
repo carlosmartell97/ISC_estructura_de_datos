6100abe80f6f0487836c2c0917c2ceaf689a8f9a
carlosmartell97 <carlosm_97@live.com>
2016-09-02 22:03:57 -0500

    making it simpler

--- 1 parcial/Midterm _Practice/src/intento.java
+++ 1 parcial/Midterm _Practice/src/intento.java
@@ -1,17 +1,11 @@
 import java.util.ArrayList;
 
 public class intento<Item> extends ArrayList<Item>{
-	private int size;
-	private ArrayList element;
-	private int maxSize=100;
-	public intento(){
-		this.size=0;
-		this.element=new ArrayList<Item>();
-	}
 	
 	public static void main(String[] args) {
 		intento<Integer> lista = new intento<Integer>();
 		lista.add(0, 1);
+		lista.add(1, 5);
 		System.out.println(lista);
 	}
 }
