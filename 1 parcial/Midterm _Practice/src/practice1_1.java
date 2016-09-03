37c411fc6c2b2d177aac6bd22a73b0e8450129a0
carlosmartell97 <carlosm_97@live.com>
2016-09-02 22:37:39 -0500

    hmmmm

--- 1 parcial/Midterm _Practice/src/practice1_1.java
+++ 1 parcial/Midterm _Practice/src/practice1_1.java
@@ -1,11 +1,24 @@
-public class practice1_1<T>{
-	private T[] element;
+public class practice1_1{
+	private int[] element;
+	
+	public practice1_1(){
+		this.element=new int[4];
+		this.element[0]=1;
+		this.element[1]=2;
+		this.element[2]=3;
+		this.element[3]=4;
+	}
 	
 	public void rightShift(int k){
-		T[] newElement = (T[]) new Object[this.element.length+k];
+		int[] newElement = new int[this.element.length+k];
 		for(int i=0;i<k;i++){
 			this.element[i]=0;
 		}
 		System.arraycopy(this.element, k, newElement, k, newElement.length);
+	}
+	
+	public static void main(String[] args) {
+		practice1_1 o = new practice1_1();
+		System.out.println(o.element.toString());
 	}
 }
