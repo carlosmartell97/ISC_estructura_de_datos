08f3d73e66ad7834e0166de5bb10c73a59779ade
carlosmartell97 <carlosm_97@live.com>
2016-09-02 22:10:41 -0500

    making it simpler

--- 1 parcial/Midterm _Practice/src/intento.java
+++ 1 parcial/Midterm _Practice/src/intento.java
@@ -5,7 +5,7 @@ public class intento<Item> extends ArrayList<Item>{
 	public static void main(String[] args) {
 		intento<Integer> lista = new intento<Integer>();
 		lista.add(0, 1);
-		lista.add(1, 5);
+		lista.add(0, 5);
 		System.out.println(lista);
 	}
 }
