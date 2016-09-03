ab9850ea03e3ce7f200293f39935f06b9f457364
carlosmartell97 <carlosm_97@live.com>
2016-09-03 17:02:45 -0500

    hasta aquí supe, después le sigo

---
+++ 1 parcial/Midterm _Practice/src/practice1_4.java
@@ -0,0 +1,18 @@
+public class practice1_4<T> {
+	private Node<T> head;
+	private Node<T> tail;
+	private int size;
+	
+	public void reverse(){
+		T temp;
+	}
+	
+	public void myQueueReverse(){
+		Node<T> temp;
+	}
+	
+	private static class Node<T>{
+		Node<T> next;
+		T content;
+	}
+}
