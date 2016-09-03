4a2cd2cd579aa774cbc42d41c2b8866c532b7dde
carlosmartell97 <carlosm_97@live.com>
2016-09-02 23:17:58 -0500

    I have an idea

--- 1 parcial/Midterm _Practice/src/practice1_2.java
+++ 1 parcial/Midterm _Practice/src/practice1_2.java
@@ -1,6 +1,16 @@
-public class practice1_2<T>{
+public class practice1_2<T extends Comparable>{
 	private ChainNode<T> firstNode;
 	private int size;
+	
+	public boolean isSorted(){
+		for(int i=0;i<this.size;i++){
+			ChainNode<T> temp = new ChainNode<T>();
+			if((temp.element.compareTo(temp.next.element))>0){
+				return false;
+			}
+			return true;
+		}
+	}
 	
 	private static class ChainNode<T>{
 		T element;
