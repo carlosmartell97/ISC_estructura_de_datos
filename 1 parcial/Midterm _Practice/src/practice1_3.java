62a18969533b625027e60c1d9a9bc70e97671388
carlosmartell97 <carlosm_97@live.com>
2016-09-03 15:48:02 -0500

    done. It works with Integers

--- 1 parcial/Midterm _Practice/src/practice1_3.java
+++ 1 parcial/Midterm _Practice/src/practice1_3.java
@@ -1,16 +1,20 @@
+//funciona con Integers.
 import java.util.Arrays;
 
-public class practice1_3 {
-	private int[][] element={
+public class practice1_3<T>{
+	private int[][] sampleElement={
 			{1,0,0,0,6},
 			{2,11,0,0,7},
 			{3,0,12,0,8},
 			{4,0,0,13,9},
 			{5,0,0,0,10}
 	};
-	private int[]compactMatrix={1,2,3,4,5,6,7,8,9,10,11,12,13};
+	private int[]sampleCompactMatrix={1,2,3,4,5,6,7,8,9,10,11,12,13};
 	
-	public void set(int i,int j,int newValue){
+	private T[][] element;
+	private T[] compactMatrix;
+	
+	public void set(int i,int j,T newValue){
 		if(i==0){
 			compactMatrix[j]=newValue;
 			System.out.println("change1");
@@ -25,8 +29,8 @@ public class practice1_3 {
 		}
 	}
 	public static void main(String[] args) {
-		practice1_3 o = new practice1_3();
-		o.set(1, 1, 66);
-		System.out.println(Arrays.toString(o.compactMatrix));
+		/*practice1_3 o = new practice1_3();
+		o.set(3, 3, 66);
+		System.out.println(Arrays.toString(o.compactMatrix));*/
 	}
 }
