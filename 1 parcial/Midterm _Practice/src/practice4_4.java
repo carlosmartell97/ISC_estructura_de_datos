cfa5acefa0a244cbc8050dbbd3831069344db521
carlosmartell97 <carlosm_97@live.com>
2016-09-04 16:42:58 -0500

    done

---
+++ 1 parcial/Midterm _Practice/src/practice4_4.java
@@ -0,0 +1,25 @@
+// resuleto. Estoy usando el stack de la recursión de mi método test(int,String)
+public class practice4_4 {
+	private String s="";
+	
+	public boolean test(String word){
+		this.s="";
+		word=word.replace(" ", "");
+		word=word.replace(",", "");
+		word=word.replace(".", "");
+		word=word.toLowerCase();
+		test(0,word);
+		return s.equals(word.substring(word.length()/2+1));
+	}
+	public void test(int i,String word){
+		if(i<word.length()/2){
+			test(i+1,word);
+			s+=word.charAt(i);
+		}
+	}
+	
+	public static void main(String[] args) {
+		practice4_4 o = new practice4_4();
+		System.out.println(o.test("amo la pacifica paloma"));
+	}
+}
