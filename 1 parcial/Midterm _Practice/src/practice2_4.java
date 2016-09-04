0d1561e1b970cd1dc5dd9777c195678315b7781b
carlosmartell97 <carlosm_97@live.com>
2016-09-04 14:55:05 -0500

    even simpler. got rid of one variable

--- 1 parcial/Midterm _Practice/src/practice2_4.java
+++ 1 parcial/Midterm _Practice/src/practice2_4.java
@@ -1,18 +1,16 @@
+// funciona con Integers. pero solo si el "n" de Q es par 
 import java.util.Arrays;
 
 public class practice2_4 {
-	private int[] Q={1,2,3,4,5,6,7,8,9,10};
+	private int[] Q={1,2,3,4,5,6,7,8};
 	
 	public void interleave(int[] queue){
 		int[] first=new int[this.Q.length/2];
 		int[] second=new int[this.Q.length/2];
 		int[] newQ=new int[this.Q.length];
-		for(int i=0,j=this.Q.length/2,contador=0; i<this.Q.length; i=i+2,j++,contador++){
-			//first[i]=Q[i];
-			//second[i]=Q[j];
-			System.out.println(j%this.Q.length/2);
+		for(int i=0,contador=0; i<this.Q.length; i=i+2,contador++){
 			newQ[i]=Q[contador];
-			newQ[i+1]=Q[j];
+			newQ[i+1]=Q[contador+this.Q.length/2];
 		}
 		System.out.println(Arrays.toString(first));
 		System.out.println(Arrays.toString(second));
