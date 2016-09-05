bbee872e1ba326fc2073452d48925877599e26cf
carlosmartell97 <carlosm_97@live.com>
2016-09-04 23:39:08 -0500

    basic testing

--- 1 parcial/Midterm _Practice/src/practice3_2.java
+++ 1 parcial/Midterm _Practice/src/practice3_2.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+
 public class practice3_2<T>{
 	T[] element;
 	public void removeNull(){
-		
+		System.out.println(Arrays.toString(element));
+	}
+	
+	public static void main(String[] args) {
+		practice3_2 o = new practice3_2();
+		o.element = new Object[3];
+		o.element[0]=1;
+		o.element[1]="dos";
+		o.element[2]=3.0;
+		o.removeNull();
 	}
 }
