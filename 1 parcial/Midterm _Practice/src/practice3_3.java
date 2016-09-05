5159747927b382cbc8c84c3eb135da4a3e5cbbf8
carlosmartell97 <carlosm_97@live.com>
2016-09-05 12:43:28 -0500

    done

--- 1 parcial/Midterm _Practice/src/practice3_3.java
+++ 1 parcial/Midterm _Practice/src/practice3_3.java
@@ -1,3 +1,4 @@
+//	ya, 100%. Necesito entender qué es lo que hace
 public class practice3_3 {
 	private MyChainNode headerNode=new MyChainNode();
 	//this.headerNode.element=INFINITY;
@@ -7,6 +8,26 @@ public class practice3_3 {
 		MyChainNode currentA=a.headerNode.next,
 					currentB=b.headerNode.next,
 					lastThis=headerNode;
+		while(currentA!=a.headerNode || currentB!=b.headerNode){
+			//	merge from a
+			if(currentA.element<=currentB.element){
+				lastThis=lastThis.next=currentA;
+				currentA=currentA.next;
+			}
+			//	merge from b
+			else{
+				lastThis=lastThis.next=currentB;
+				currentB=currentB.next;
+			}
+		}
+		//	now, close circular list and update size
+		lastThis.next=headerNode;
+		size=a.size+b.size;
+		
+		//	empty a and b
+		a.headerNode.next=a.headerNode;
+		b.headerNode.next=b.headerNode;
+		a.size=b.size=0;
 	}
 	
 	private static class MyChainNode{
