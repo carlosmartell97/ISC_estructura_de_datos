fd41fb3593f40bbcddcedeb961184c1cc7a3db04
carlosmartell97 <carlosm_97@live.com>
2016-09-05 09:53:50 -0500

    working

--- 1 parcial/Midterm _Practice/src/practice3_3.java
+++ 1 parcial/Midterm _Practice/src/practice3_3.java
@@ -1,6 +1,13 @@
 public class practice3_3 {
-	private MyChainNode headerNode;
+	private MyChainNode headerNode=new MyChainNode();
+	//this.headerNode.element=INFINITY;
 	private int size; //	excludes the headerNode
+	
+	public void merge(practice3_3 a,practice3_3 b){
+		MyChainNode currentA=a.headerNode.next,
+					currentB=b.headerNode.next,
+					lastThis=headerNode;
+	}
 	
 	private static class MyChainNode{
 		int element;
