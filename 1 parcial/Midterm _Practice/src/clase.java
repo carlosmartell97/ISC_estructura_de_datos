3e17dbee897d9d3d2cea96940336b8a133afad07
carlosmartell97 <carlosm_97@live.com>
2016-09-05 18:07:16 -0500

    probando

---
+++ 1 parcial/Midterm _Practice/src/clase.java
@@ -0,0 +1,9 @@
+public class clase {
+	public clase(){
+		//this
+	}
+	public static void main(String[] args) {
+		//System.out.println(Math.log(8)/Math.log(2));
+		System.out.println(Math.ceil(2.1));
+	}
+}
