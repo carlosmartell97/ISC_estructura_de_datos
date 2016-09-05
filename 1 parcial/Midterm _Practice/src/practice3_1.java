8bd4d521b5d237bb4215211c35048cac655658e9
carlosmartell97 <carlosm_97@live.com>
2016-09-04 23:27:55 -0500

    making progress

--- 1 parcial/Midterm _Practice/src/practice3_1.java
+++ 1 parcial/Midterm _Practice/src/practice3_1.java
@@ -2,17 +2,33 @@ import java.util.Iterator;
 
 public class practice3_1 {
 	
+	public void clear(){
+		
+	}
+	
+	public void append(Object newObject){
+		
+	}
+	
+	public Iterator iterator(){
+		return new inner();
+	}
+	
+	public static void split(practice3_1 a,practice3_1 b,practice3_1 c){
+		
+	}
+	
 	private static class inner implements Iterator{
-		@Override
 		public boolean hasNext() {
 			// TODO Auto-generated method stub
 			return false;
 		}
-		@Override
 		public Object next() {
 			// TODO Auto-generated method stub
 			return null;
 		}
-		
+		/*public practice3_1 remove(){
+			
+		}*/
 	}
 }
