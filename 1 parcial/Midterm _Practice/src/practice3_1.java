ef3dfbe8201c61c5a233e504e2f8e9c09d188eb3
carlosmartell97 <carlosm_97@live.com>
2016-09-04 23:14:09 -0500

    basic structure 50%

---
+++ 1 parcial/Midterm _Practice/src/practice3_1.java
@@ -0,0 +1,18 @@
+import java.util.Iterator;
+
+public class practice3_1 {
+	
+	private static class inner implements Iterator{
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+		@Override
+		public Object next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+}
