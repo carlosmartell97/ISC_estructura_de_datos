46f64bb93ec18802dcf713a31b6c8fe2be8f8067
carlosmartell97 <carlosm_97@live.com>
2016-09-05 09:53:01 -0500

    mmmm making progress

---
+++ 1 parcial/Midterm _Practice/src/practice3_3.java
@@ -0,0 +1,9 @@
+public class practice3_3 {
+	private MyChainNode headerNode;
+	private int size; //	excludes the headerNode
+	
+	private static class MyChainNode{
+		int element;
+		MyChainNode next;
+	}
+}
