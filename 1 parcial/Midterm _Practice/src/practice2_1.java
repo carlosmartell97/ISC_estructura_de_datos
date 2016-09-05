4404501c6f8eb6454caf781e61c0beafafc458b3
carlosmartell97 <carlosm_97@live.com>
2016-09-04 20:33:51 -0500

    done 100%

--- 1 parcial/Midterm _Practice/src/practice2_1.java
+++ 1 parcial/Midterm _Practice/src/practice2_1.java
@@ -5,16 +5,22 @@ public class practice2_1<T>{
 	private T[] element;
 	
 	public void compress(){
-		T[] newElement=(T[]) new Object[(1+this.element.length)/2];
-		for(int i=0,x=0;i<this.element.length;i=i+2,x=x+1){
-			newElement[x]=this.element[i];
+		T[] newElement=(T[]) new Object[(1+this.element.length)/2];	//	O(1)
+		for(int i=0,x=0;i<this.element.length;i=i+2,x++){	//	O(n)
+			newElement[x]=this.element[i];	//	O(1)
 		}
-		this.element=newElement;
+		this.element=newElement;	//	O(1)
 	}
 	
 	public static void main(String[] args) {
-		/*practice2_1 o = new practice2_1();
+		practice2_1 o = new practice2_1();
+		o.element=new Object[5];
+		o.element[0]=1;
+		o.element[1]="dos";
+		o.element[2]=3.0;
+		o.element[3]=4;
+		o.element[4]="hola";
 		o.compress();
-		System.out.println(Arrays.toString(o.element));*/
+		System.out.println(Arrays.toString(o.element));
 	}
 }
