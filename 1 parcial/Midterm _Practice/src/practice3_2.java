113694dbce66fac524c47c4b81827832f4986a63
carlosmartell97 <carlosm_97@live.com>
2016-09-05 07:55:16 -0500

    done 100%

--- 1 parcial/Midterm _Practice/src/practice3_2.java
+++ 1 parcial/Midterm _Practice/src/practice3_2.java
@@ -4,17 +4,37 @@
 import java.util.Arrays;
 
 public class practice3_2<T>{
-	T[] element;
+	private T[] element;
+	private int size;
+	
 	public void removeNull(){
-		System.out.println(Arrays.toString(element));
+		int newSize=0;
+		for(int i=0;i<this.element.length;i++){
+			if(this.element[i]!=null){
+				this.element[newSize++]=this.element[i];
+			}
+		}
+		for(int i=newSize;i<this.element.length;i++){
+			this.element[i]=null;
+		}
+		this.size=newSize;
+		
+		System.out.println("for:");
+		for(int i=0;i<this.size;i++){
+			System.out.println(this.element[i]);
+		}
+		System.out.println("one time:");
+		System.out.println(Arrays.toString(this.element));
 	}
 	
 	public static void main(String[] args) {
 		practice3_2 o = new practice3_2();
-		o.element = new Object[3];
+		o.element = new Object[5];
 		o.element[0]=1;
 		o.element[1]="dos";
-		o.element[2]=3.0;
+		o.element[2]=null;
+		o.element[3]=3.0;
+		o.element[4]=null;
 		o.removeNull();
 	}
 }
