4f2b54158fb2987aadc7144a631dca90512158cc
carlosmartell97 <carlosm_97@live.com>
2016-09-16 21:06:23 -0500

    added main() and I had a weird error on console...

--- 2 parcial/3rd_Homework/src/Utilities/Conversion.java
+++ 2 parcial/3rd_Homework/src/Utilities/Conversion.java
@@ -5,11 +5,18 @@ public class Conversion {
 	public static void Recursive(ArrayLinearList<Character> list){
 		ArrayLinearList<Character> listReturned=new ArrayLinearList<Character>();
 		ArrayLinearList<Character> operators=new ArrayLinearList<Character>();
+		
+		Recursive(list.size,list);
 	}
-	public static void Recursive(int position,ArrayLinearList<Character> list){
+	protected static void Recursive(int position,ArrayLinearList<Character> list){
 		if(position<list.size-1){
 			System.out.println("call");
 			Recursive(position+1,list);
 		}
+	}
+	
+	public static void main(String[] args) {
+		ArrayLinearList<Character> lista=new ArrayLinearList<Character>();
+		Recursive(lista);
 	}
 }
