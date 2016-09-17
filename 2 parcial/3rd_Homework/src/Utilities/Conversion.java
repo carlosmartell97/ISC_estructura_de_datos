8f0562cc6773ad8a621ac4aad4516fe11f1a2b86
carlosmartell97 <carlosm_97@live.com>
2016-09-16 21:02:13 -0500

    basic structure

--- 2 parcial/3rd_Homework/src/Utilities/Conversion.java
+++ 2 parcial/3rd_Homework/src/Utilities/Conversion.java
@@ -1,7 +1,15 @@
 package Utilities;
 
 public class Conversion {
-	public static void Recursive(ArrayLinearList<Integer> list){
-		
+	
+	public static void Recursive(ArrayLinearList<Character> list){
+		ArrayLinearList<Character> listReturned=new ArrayLinearList<Character>();
+		ArrayLinearList<Character> operators=new ArrayLinearList<Character>();
+	}
+	public static void Recursive(int position,ArrayLinearList<Character> list){
+		if(position<list.size-1){
+			System.out.println("call");
+			Recursive(position+1,list);
+		}
 	}
 }
