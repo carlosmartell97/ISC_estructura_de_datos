5b71a4b8d50c58aa74ed4cff5765f26ce10890a6
carlosmartell97 <carlosm_97@live.com>
2016-09-17 00:40:22 -0500

    now I can't get around a Casting error...

--- 2 parcial/3rd_Homework/src/Utilities/Conversion.java
+++ 2 parcial/3rd_Homework/src/Utilities/Conversion.java
@@ -1,22 +1,32 @@
 package Utilities;
 
 public class Conversion {
+	protected static ArrayLinearList<Character> listReturned=new ArrayLinearList<Character>();
+	protected static ArrayLinearList<Character> operators=new ArrayLinearList<Character>();
+	protected static char item;
 	
 	public static void Recursive(ArrayLinearList<Character> list){
-		ArrayLinearList<Character> listReturned=new ArrayLinearList<Character>();
-		ArrayLinearList<Character> operators=new ArrayLinearList<Character>();
-		
-		Recursive(list.size,list);
+		Recursive(0,list);
 	}
 	protected static void Recursive(int position,ArrayLinearList<Character> list){
-		if(position<list.size-1){
-			System.out.println("call");
+		if(position<list.size){
+			System.out.println("CALL");
+			//item=new Character((Character)list.element[position]);
+			//item=Object.toString().charAt(item);
+			
+			if(Character.isAlphabetic(list.remove(0))){
+				System.out.println("true");
+			}
 			Recursive(position+1,list);
 		}
 	}
 	
 	public static void main(String[] args) {
 		ArrayLinearList<Character> lista=new ArrayLinearList<Character>();
+		lista.add(0, '5');
+		lista.add(0, '4');
+		lista.add(0, '3');
 		Recursive(lista);
+		System.out.println(lista);
 	}
 }
