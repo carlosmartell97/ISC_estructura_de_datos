4ce4f9a3dafe8459eac962a6e11847b7d461c611
carlosmartell97 <carlosm_97@live.com>
2016-09-21 17:28:24 -0500

    It should work now, with % on position. It still has a CastException, though

--- 2 parcial/3rd_Homework/src/Utilities/Conversion.java
+++ 2 parcial/3rd_Homework/src/Utilities/Conversion.java
@@ -1,21 +1,26 @@
 package Utilities;
 
 public class Conversion {
-	protected static ArrayLinearList<Character> listReturned=new ArrayLinearList<Character>();
-	protected static ArrayLinearList<Character> operators=new ArrayLinearList<Character>();
+	protected static ArrayLinearList<Character> listReturned;
+	protected static ArrayLinearList<Character> operators;
 	protected static char item;
 	
-	public static void Recursive(ArrayLinearList<Character> list){
+	public static ArrayLinearList<Character> Recursive(ArrayLinearList<Character> list){
+		listReturned=new ArrayLinearList<Character>();
+		operators=new ArrayLinearList<Character>();
 		Recursive(0,list);
+		for(int i=0;i<operators.size;i++){
+			listReturned.add(listReturned.size, operators.get(i));
+		}
+		return listReturned;
 	}
 	protected static void Recursive(int position,ArrayLinearList<Character> list){
 		if(position<list.size){
 			System.out.println("CALL");
-			//item=new Character((Character)list.element[position]);
-			//item=Object.toString().charAt(item);
-			
-			if(Character.isAlphabetic(list.remove(0))){
-				System.out.println("true");
+			if(position%2==0){
+				listReturned.add(listReturned.size, list.element[position]);
+			}else{
+				operators.add(operators.size, list.element[position]);
 			}
 			Recursive(position+1,list);
 		}
@@ -26,7 +31,7 @@ public class Conversion {
 		lista.add(0, '5');
 		lista.add(0, '4');
 		lista.add(0, '3');
-		Recursive(lista);
 		System.out.println(lista);
+		//Recursive(lista);
 	}
 }
