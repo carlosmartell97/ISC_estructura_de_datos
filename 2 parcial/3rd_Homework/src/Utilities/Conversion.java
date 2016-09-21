7ec50a5a34616b4e6a86062e83dd59dc0a88460a
carlosmartell97 <carlosm_97@live.com>
2016-09-21 18:10:08 -0500

    I still have CastExceptions, but my logic works

--- 2 parcial/3rd_Homework/src/Utilities/Conversion.java
+++ 2 parcial/3rd_Homework/src/Utilities/Conversion.java
@@ -2,36 +2,68 @@ package Utilities;
 
 public class Conversion {
 	protected static ArrayLinearList<Character> listReturned;
-	protected static ArrayLinearList<Character> operators;
+	protected static ArrayLinearList<Character> listOperators;
+	protected static Stack<Character> stackOperands;
+	protected static Stack<Character> stackOperators;
 	protected static char item;
 	
 	public static ArrayLinearList<Character> Recursive(ArrayLinearList<Character> list){
+		if(list.isEmpty()){
+			return null;
+		}
 		listReturned=new ArrayLinearList<Character>();
-		operators=new ArrayLinearList<Character>();
-		Recursive(0,list);
-		for(int i=0;i<operators.size;i++){
-			listReturned.add(listReturned.size, operators.get(i));
+		listOperators=new ArrayLinearList<Character>();
+		Recursive(list.size-1,list);
+		for(int i=0;i<listOperators.size;i++){
+			listReturned.add(listReturned.size, listOperators.get(i));
 		}
 		return listReturned;
 	}
-	protected static void Recursive(int position,ArrayLinearList<Character> list){
-		if(position<list.size){
-			System.out.println("CALL");
+	private static void Recursive(int position,ArrayLinearList<Character> list){
+		if(position>=0){
+			//System.out.println("CALL");
 			if(position%2==0){
 				listReturned.add(listReturned.size, list.element[position]);
 			}else{
-				operators.add(operators.size, list.element[position]);
+				listOperators.add(listOperators.size, list.element[position]);
 			}
-			Recursive(position+1,list);
+			Recursive(position-1,list);
 		}
+	}
+	
+	public static ArrayLinearList<Character> Iterative(ArrayLinearList<Character> list){
+		if(list.isEmpty()){
+			return null;
+		}
+		listReturned=new ArrayLinearList<Character>();
+		stackOperands=new Stack<Character>();
+		stackOperators=new Stack<Character>();
+		for(int i=0;i<list.size;i++){
+			if(i%2==0){
+				stackOperands.push(list.element[i]);
+			}
+			else{
+				stackOperators.push(list.element[i]);
+			}
+		}
+		while(!stackOperands.isEmpty()){
+			listReturned.add(listReturned.size, stackOperands.pop());
+		}
+		while(!stackOperators.isEmpty()){
+			listReturned.add(listReturned.size, stackOperators.pop());
+		}
+		return listReturned;
 	}
 	
 	public static void main(String[] args) {
 		ArrayLinearList<Character> lista=new ArrayLinearList<Character>();
 		lista.add(0, '5');
-		lista.add(0, '4');
+		lista.add(0, '-');
 		lista.add(0, '3');
+		lista.add(0, '+');
+		lista.add(0, '1');
 		System.out.println(lista);
 		//Recursive(lista);
+		Iterative(lista);
 	}
 }
