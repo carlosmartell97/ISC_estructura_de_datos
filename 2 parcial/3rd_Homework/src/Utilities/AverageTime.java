7644873820a947f174118e7bc1f0f99c8d8082f2
carlosmartell97 <carlosm_97@live.com>
2016-10-04 22:35:35 -0500

    added time complexity tests for Evaluation class

--- 2 parcial/3rd_Homework/src/Utilities/AverageTime.java
+++ 2 parcial/3rd_Homework/src/Utilities/AverageTime.java
@@ -17,11 +17,20 @@ public class AverageTime {
 		lista.add(0, '+');
 		lista.add(0, '-');
 		lista.add(0, '+');
-		System.out.println(lista);
+		System.out.println("lista: "+lista);
+		
+		ChainLinearList<Character> chain=new ChainLinearList<Character>();
+		chain.add(0, '1');
+		chain.add(0, '2');
+		chain.add(0, '3');
+		chain.add(0, '-');
+		chain.add(0, '+');
+		System.out.println("chain: "+chain);
 		
 		try {
 			BufferedWriter write=new BufferedWriter(new FileWriter("Readme1.txt"));
 			
+			System.out.println("for the class Conversion...");
 			int average;
 			for(int i=0;i<20;i++){
 				startTime=System.nanoTime();
@@ -30,13 +39,36 @@ public class AverageTime {
 				average=+totalTime;
 			}
 			System.out.println("avrg nanoTime Recursive: "+totalTime/20);
-			write.write("avrg nanoTime Recursive: "+totalTime/20+"\n");
+			write.write("avrg nanoTime Recursive: "+totalTime/20);
 			write.newLine();
 			
 			average=0;
 			for(int i=0;i<20;i++){
 				startTime=System.nanoTime();
 				Conversion.Iterative(lista);
+				totalTime=(int) (System.nanoTime()-startTime);
+				average=+totalTime;
+			}
+			System.out.println("avrg nanoTime Iterative: "+totalTime/20);
+			write.write("avrg nanoTime Iterative: "+totalTime/20);
+			
+			////////////////////////////////////////////////////////////////////////////////////
+			System.out.println("for the class Evaluation...");
+			average=0;
+			for(int i=0;i<20;i++){
+				startTime=System.nanoTime();
+				Evaluation.Recursive(chain);
+				totalTime=(int) (System.nanoTime()-startTime);
+				average=+totalTime;
+			}
+			System.out.println("avrg nanoTime Recursive: "+totalTime/20);
+			write.write("avrg nanoTime Recursive: "+totalTime/20);
+			write.newLine();
+			
+			average=0;
+			for(int i=0;i<20;i++){
+				startTime=System.nanoTime();
+				Evaluation.Iterative(chain);
 				totalTime=(int) (System.nanoTime()-startTime);
 				average=+totalTime;
 			}
