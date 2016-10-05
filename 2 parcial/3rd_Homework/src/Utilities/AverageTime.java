551553abf7238c1267cee77de0673fd9c17fd79d
carlosmartell97 <carlosm_97@live.com>
2016-10-04 22:17:50 -0500

    removed what wasn't needed. Added what was

--- 2 parcial/3rd_Homework/src/Utilities/AverageTime.java
+++ 2 parcial/3rd_Homework/src/Utilities/AverageTime.java
@@ -3,69 +3,43 @@ package Utilities;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Random;
 
 public class AverageTime {
 	
 	public static void main(String[] args) {
-		long startTime,
-				endTime=0;
-		int timeSumAdd=0,
-				timeSumRemove=0;
-		ArrayLinearList<Integer> array = new ArrayLinearList();
-		ChainLinearList<Integer> chain = new ChainLinearList();
-		int[] testing=new int[10];
-		for(int i=0;i<testing.length;i++){
-			testing[i]=new Random().nextInt(123456789)+1;
-		}
+		long startTime;
+		int totalTime=0;
+		ArrayLinearList<Character> lista=new ArrayLinearList<Character>();
+		lista.add(0, 'D');
+		lista.add(0, 'Z');
+		lista.add(0, 'Y');
+		lista.add(0, 'X');
+		lista.add(0, '+');
+		lista.add(0, '-');
+		lista.add(0, '+');
+		System.out.println(lista);
 		
 		try {
-			BufferedWriter write=new BufferedWriter(new FileWriter("AverageTimeChainArray.txt"));
-			//________________________________________________________
-			for(int times=0;times<20;times++){	
+			BufferedWriter write=new BufferedWriter(new FileWriter("Readme1.txt"));
+			
+			int average;
+			for(int i=0;i<20;i++){
 				startTime=System.nanoTime();
-				for(int i=0;i<testing.length;i++){
-					array.add(0,testing[i]);
-				}
-				endTime=System.nanoTime()-startTime;
-				timeSumAdd+=endTime;
-				
-				startTime=System.nanoTime();
-				for(int i=0;i<array.size;i++){
-					array.remove(0);
-				}
-				endTime=System.nanoTime()-startTime;
-				timeSumRemove+=endTime;
+				Conversion.Recursive(lista);
+				totalTime=(int) (System.nanoTime()-startTime);
+				average=+totalTime;
 			}
-			System.out.println("average nanoTime in array add: "+timeSumAdd/20);
-			write.write("average nanoTime in array add: "+timeSumAdd/20);	write.newLine();
+			System.out.println("avrg nanoTime Recursive: "+totalTime/20);
 			
-			System.out.println("average nanoTime in array remove: "+timeSumRemove/20);
-			write.write("average nanoTime in array remove: "+timeSumRemove/20); write.newLine();
-			
-			timeSumAdd=0;timeSumRemove=0;
-			//////////////////////////////////////////////////////////
-			for(int times=0;times<20;times++){
+			average=0;
+			for(int i=0;i<20;i++){
 				startTime=System.nanoTime();
-				for(int i=0;i<testing.length;i++){
-					chain.add(0,testing[i]);
-				}
-				endTime=System.nanoTime()-startTime;
-				timeSumAdd+=endTime;
-				
-				startTime=System.nanoTime();
-				for(int i=0;i<chain.size;i++){
-					chain.remove(0);
-				}
-				endTime=System.nanoTime()-startTime;
-				timeSumRemove+=endTime;
+				Conversion.Iterative(lista);
+				totalTime=(int) (System.nanoTime()-startTime);
+				average=+totalTime;
 			}
-			System.out.println("average nanoTime in chain add: "+timeSumAdd/20);
-			write.write("average nanoTime in chain add: "+timeSumAdd/20); write.newLine();
-			
-			System.out.println("average nanoTime in chain remove: "+timeSumRemove/20);
-			write.write("average nanoTime in chain remove: "+timeSumRemove/20); write.newLine();
-			//________________________________________________________
+			System.out.println("avrg nanoTime Iterative: "+totalTime/20);
+			//write.write("average nanoTime in chain remove: "+timeSumRemove/20); write.newLine();
 			write.close();
 		} catch (IOException e) {
 			e.getMessage();
