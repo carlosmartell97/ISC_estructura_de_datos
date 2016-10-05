fb2732ab43d0bff26a12318ea6d8d2e59843bad4
carlosmartell97 <carlosm_97@live.com>
2016-10-05 12:18:57 -0500

    it should be working now

--- 2 parcial/3rd_Homework/src/Utilities/Check.java
+++ 2 parcial/3rd_Homework/src/Utilities/Check.java
@@ -3,5 +3,17 @@
 package Utilities;
 
 public class Check {
-
+	private Stack<Character> unos; 
+	public boolean Iterative(ChainLinearList<Character> list){
+		if(list.size%2!=0){
+			return false;
+		}
+		unos=new Stack<Character>();
+		for(int i=0;i<list.size;i++){
+			if(list.get(i)=='1'){
+				unos.push(list.get(i));
+			}
+		}
+		return unos.size()==list.size/2;
+	}
 }
