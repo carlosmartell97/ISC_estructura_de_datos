425db777584b1c0ee3e8727275f6181230593d42
carlosmartell97 <carlosm_97@live.com>
2016-10-07 20:40:33 -0500

    added add() and constructor method for ChainNode

--- 1 parcial/Midterm _Practice/src/practice1_2.java
+++ 1 parcial/Midterm _Practice/src/practice1_2.java
@@ -5,7 +5,7 @@ public class practice1_2<T extends Comparable>{
 	
 	public boolean isSorted(){
 		ChainNode<T> temp = firstNode;
-		for(int i=0;i<this.size;i++){	// O(n)
+		while(temp.next!=null){	// O(n)
 			if((temp.element.compareTo(temp.next.element))>0){	// O(n-1)
 				return false;
 			}
@@ -14,8 +14,39 @@ public class practice1_2<T extends Comparable>{
 		return true;
 	}
 	
+	public void add(int index, T item) {
+		ChainNode<T> newNode=new ChainNode<T> (item);
+		if(index<0 || index>this.size){
+			throw new IndexOutOfBoundsException("that index is out of bounds, dude...");
+		}
+		else if(index==0){
+			newNode.next=this.firstNode;
+			this.firstNode=newNode;
+			this.size++;
+		}
+		else{
+			ChainNode<T> anterior= this.firstNode;
+			ChainNode<T> siguiente;
+			for(int i=0; i<index-1; i++){
+				anterior=anterior.next;
+			}
+			siguiente=anterior.next;
+			newNode.next=siguiente;
+			anterior.next=newNode;
+			this.size++;
+		}
+	}
+	
 	private static class ChainNode<T>{
 		T element;
 		ChainNode<T> next;
+		
+		public ChainNode(T element){
+			this.element=element;
+		}
+	}
+	
+	public static void main(String[] args) {
+		practice1_2 o = new practice1_2();
 	}
 }
