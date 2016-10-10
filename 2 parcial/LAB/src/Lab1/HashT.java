81674365e1df6e658f390609a4cdf980f2c31033
carlosmartell97 <carlosm_97@live.com>
2016-10-10 17:00:11 -0500

    moved testing to new class, HashT_Testing.java

--- 2 parcial/LAB/src/Lab1/HashT.java
+++ 2 parcial/LAB/src/Lab1/HashT.java
@@ -211,27 +211,4 @@ public class HashT<K,V> {
 			throw new NoSuchElementException("there's no nextNode");
 		}
 	}
-	
-	public static void main(String[] args) {
-		HashT<String,Integer> table=new HashT<String,Integer>();
-		
-		table.add("dos + dos", 4); System.out.println("size: "+table.size());
-		table.add("dos + cuatro", 6); System.out.println("size: "+table.size());
-		table.add("ocho - uno", 7); System.out.println("size: "+table.size());
-		table.add("cinco + tres", 8); System.out.println("size: "+table.size());
-		System.out.println("remove: "+table.remove("dos + dos")); System.out.println("size: "+table.size());
-		table.add("diez - cinco", 5); System.out.println("size: "+table.size());
-		
-		Iterator<String> key=table.getKeyIterator();
-		System.out.println(key.next());
-		if(key.hasNext()){
-			System.out.println(key.next());
-		}
-		
-		Iterator<Integer> value=table.getValueIterator();
-		System.out.println(value.next());
-		if(key.hasNext()){
-			System.out.println(value.next());
-		}
-	}
 }
