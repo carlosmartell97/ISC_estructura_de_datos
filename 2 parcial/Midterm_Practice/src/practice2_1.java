799603d8535f1db0e3150d537a4e03c6ee126630
carlosmartell97 <carlosm_97@live.com>
2016-10-16 17:24:18 -0500

    I believe recursion is the way to go...

--- 2 parcial/Midterm_Practice/src/practice2_1.java
+++ 2 parcial/Midterm_Practice/src/practice2_1.java
@@ -8,7 +8,15 @@ public class practice2_1 {
 		BinaryTreeNode root;
 		
 		public Object elementAtLevel(int level){
-			
+			find(this.root,level,1);
+		}
+		
+		private void find(BinaryTreeNode root,int level,int currentLevel){
+			if(root!=null){
+				
+				find(root.left,level,++currentLevel);
+				find(root.left,level,++currentLevel);
+			}
 		}
 	}
 }
