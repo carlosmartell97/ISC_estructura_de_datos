55d3cd1d32cea86051bb05903be9a3ee2413a42f
carlosmartell97 <carlosm_97@live.com>
2016-10-16 17:15:53 -0500

    basic structure

---
+++ 2 parcial/Midterm_Practice/src/practice2_1.java
@@ -0,0 +1,14 @@
+public class practice2_1 {
+	public class BinaryTreeNode{
+		Object element;
+		BinaryTreeNode	left,
+						right;
+	}
+	public class LinkedBinaryTree{
+		BinaryTreeNode root;
+		
+		public Object elementAtLevel(int level){
+			
+		}
+	}
+}
