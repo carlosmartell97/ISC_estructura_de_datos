629230b01dca5b8b59334cc11a54f941fa399d92
carlosmartell97 <carlosm_97@live.com>
2016-10-16 18:29:27 -0500

    all should be working now

--- 2 parcial/Midterm_Practice/src/practice2_1.java
+++ 2 parcial/Midterm_Practice/src/practice2_1.java
@@ -6,16 +6,24 @@ public class practice2_1 {
 	}
 	public class LinkedBinaryTree{
 		BinaryTreeNode root;
+		private Object toBeReturned;
 		
 		public Object elementAtLevel(int level){
+			toBeReturned=null;
 			find(this.root,level,1);
+			return toBeReturned;
 		}
 		
 		private void find(BinaryTreeNode root,int level,int currentLevel){
 			if(root!=null){
-				
+				if(currentLevel==level){
+					toBeReturned=root.element;
+					return;
+				}
+				//	go to the left-most node
 				find(root.left,level,++currentLevel);
-				find(root.left,level,++currentLevel);
+				//	go to the right-most node
+				find(root.right,level,++currentLevel);
 			}
 		}
 	}
