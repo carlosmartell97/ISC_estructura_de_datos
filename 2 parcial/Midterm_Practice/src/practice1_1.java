3313170d5a0cc4dc540bee7bfd550912104c5b84
carlosmartell97 <carlosm_97@live.com>
2016-10-16 14:23:25 -0500

    basic structure

---
+++ 2 parcial/Midterm_Practice/src/practice1_1.java
@@ -0,0 +1,14 @@
+
+public class practice1_1 {
+	public void swapChildren(){
+		swapChildren(root);
+	}
+	
+	private static void swapChildren(BinaryTreeNode node){
+		if(node!=null){
+			
+		}
+	}
+	
+	
+}
