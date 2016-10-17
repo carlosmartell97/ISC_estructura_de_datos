b062be822f1990b2e4ac49bd0db3ac991e16f7bf
carlosmartell97 <carlosm_97@live.com>
2016-10-16 21:30:41 -0500

    basic structure

---
+++ 2 parcial/Midterm_Practice/src/practice3_2.java
@@ -0,0 +1,11 @@
+//
+import java.util.Queue;
+
+public class practice3_2 {
+	public static Queue reverseFirstK(int k,Queue queue){
+		if(queue.isEmpty()){
+			throw new IllegalArgumentException("queue is empty!");
+		}
+		
+	}
+}
