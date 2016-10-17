54e16051e178b2ffe47d3e17741713f7b89db1c4
carlosmartell97 <carlosm_97@live.com>
2016-10-16 22:28:21 -0500

    should be working now!

--- 2 parcial/Midterm_Practice/src/practice3_2.java
+++ 2 parcial/Midterm_Practice/src/practice3_2.java
@@ -1,11 +1,27 @@
-//
+//	debería ya de funcionar. Podría hacer testing cuando tenga tiempo...
+import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class practice3_2 {
 	public static Queue reverseFirstK(int k,Queue queue){
 		if(queue.isEmpty()){
 			throw new IllegalArgumentException("queue is empty!");
 		}
+		Stack flip=new Stack();
+		Queue toBeReturned=new LinkedList();
 		
+		for(int i=0;i<k;i++){
+			flip.push(queue.remove());
+		}
+		
+		while(!flip.isEmpty()){
+			toBeReturned.add(flip.pop());
+		}
+		while(!queue.isEmpty()){
+			toBeReturned.add(queue.remove());
+		}
+		
+		return toBeReturned;
 	}
 }
