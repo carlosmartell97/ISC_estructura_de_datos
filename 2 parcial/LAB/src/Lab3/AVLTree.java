231b84bf946f16cc208d1ee19cb4d8a148752a21
carlosmartell97 <carlosm_97@live.com>
2016-10-26 09:35:25 -0500

    added get(K), which works the same as a BST

--- 2 parcial/LAB/src/Lab3/AVLTree.java
+++ 2 parcial/LAB/src/Lab3/AVLTree.java
@@ -4,6 +4,7 @@ public class AVLTree<K,V> {
 	private AVLNode root;
 	
 	public AVLNode rotateLeft(AVLNode root){
+		//	al final, el nodo que regresa debería de ser el nuevo nodo en ese punto
 		AVLNode right=root.right;
 		root.right=right.left;
 		right.left=root;
