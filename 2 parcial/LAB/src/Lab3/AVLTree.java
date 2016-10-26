7955207de569de2fa127bdc128d70f75642787af
carlosmartell97 <carlosm_97@live.com>
2016-10-26 09:28:53 -0500

    rotateLeft added. The node it returns is still left to be turned into the new root at that point

--- 2 parcial/LAB/src/Lab3/AVLTree.java
+++ 2 parcial/LAB/src/Lab3/AVLTree.java
@@ -3,6 +3,13 @@ package Lab3;
 public class AVLTree<K,V> {
 	private AVLNode root;
 	
+	public AVLNode rotateLeft(AVLNode root){
+		AVLNode right=root.right;
+		root.right=right.left;
+		right.left=root;
+		return right;
+	}
+	
 	private class AVLNode{
 		K key;
 		V value;
