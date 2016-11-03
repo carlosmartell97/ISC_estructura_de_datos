ef0ddce82057cea18f2dc6d2a3646e54352f3c9c
carlosmartell97 <carlosm_97@live.com>
2016-11-03 08:13:16 -0600

    added unimplemented methods

--- 2 parcial/4th_Homework/src/MyQueue/MyScratchQueue.java
+++ 2 parcial/4th_Homework/src/MyQueue/MyScratchQueue.java
@@ -2,4 +2,52 @@ package MyQueue;
 
 public class MyScratchQueue implements Queue<Item>{
 
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Item front() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item rear() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void enqueue(Item item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Item dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(Item item) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
