19d7a816122e1ae8875fa07fe89fb9972419f0e8
carlosmartell97 <carlosm_97@live.com>
2016-11-03 08:09:26 -0600

    added methods for interface

--- 2 parcial/4th_Homework/src/MyQueue/Queue.java
+++ 2 parcial/4th_Homework/src/MyQueue/Queue.java
@@ -1,5 +1,13 @@
 package MyQueue;
 
 public interface Queue<Item> {
-	
+	public boolean isEmpty();
+	public int size();
+	public Item front();
+	public Item rear();
+	public void enqueue(Item item);
+	public Item dequeue();
+	public Item get(int index);
+	public int indexOf(Item item);
+	public String toString();
 }
