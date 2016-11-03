4d000b235607360bd06662253d9713ffcb3dc889
carlosmartell97 <carlosm_97@live.com>
2016-11-03 14:38:04 -0600

    about to complete/change RadixSort() from slides

--- 2 parcial/4th_Homework/src/MyQueue/MyRadixSort.java
+++ 2 parcial/4th_Homework/src/MyQueue/MyRadixSort.java
@@ -2,7 +2,21 @@ package MyQueue;
 
 public class MyRadixSort {
 	
-	public static void RadixSort(){
+	public static void RadixSort(LinearList Element,int k){
+		int radix=10;
+		int power=1;
+		int digit;
+		Queue<Integer>[] digitQueue=new Queue[10];
 		
+		for(int i=0;i<k;i++){
+			for(int j=0;j<Element.size();i++){
+				digit=Element.remove(0);
+				digitQueue[(digit/power)%10].enqueue(digit);
+			}
+			for(int j=0;j<radix;j++){
+				//
+			}
+			power*=10;
+		}
 	}
 }
