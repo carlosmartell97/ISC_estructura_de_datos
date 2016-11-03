befb5624c526ddba5f333a2e3cceadc299f9095f
carlosmartell97 <carlosm_97@live.com>
2016-11-03 14:56:21 -0600

    adapted code to my current needs

--- 2 parcial/4th_Homework/src/MyQueue/MyRadixSort.java
+++ 2 parcial/4th_Homework/src/MyQueue/MyRadixSort.java
@@ -1,20 +1,26 @@
+//	ya debería de funcionar todo
 package MyQueue;
 
 public class MyRadixSort {
 	
-	public static void RadixSort(LinearList Element,int k){
+	public static ChainLinearList<Integer> RadixSort(ChainLinearList<Integer> nums,int k){
 		int radix=10;
 		int power=1;
 		int digit;
-		Queue<Integer>[] digitQueue=new Queue[10];
+		MyScratchQueue<Integer>[] digitQueue=new MyScratchQueue[10];
+		for(int i=0;i<10;i++){
+			digitQueue[i]=new MyScratchQueue<Integer>();
+		}
 		
 		for(int i=0;i<k;i++){
-			for(int j=0;j<Element.size();i++){
-				digit=Element.remove(0);
+			for(int j=0;j<nums.size();j++){
+				digit=nums.remove(0);
 				digitQueue[(digit/power)%10].enqueue(digit);
 			}
 			for(int j=0;j<radix;j++){
-				//
+				while(!digitQueue[j].isEmpty()){
+					nums.add(nums.size(), digitQueue[j].dequeue());
+				}
 			}
 			power*=10;
 		}
