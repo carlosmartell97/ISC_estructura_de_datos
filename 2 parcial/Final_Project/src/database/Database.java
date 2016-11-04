9227cd9a46c8b1bee0c6a6a6857139f38b04c29f
carlosmartell97 <carlosm_97@live.com>
2016-11-04 16:19:23 -0600

    Update Database.java
    running into problems with getAddress

--- 2 parcial/Final_Project/src/database/Database.java
+++ 2 parcial/Final_Project/src/database/Database.java
@@ -3,5 +3,32 @@ package database;
 import java.util.HashMap;
 
 public class Database {
-	HashMap<User,String> users;//tabla hash de usuarios
+	protected static HashMap<User,String> users=new HashMap();
+	
+	public String getAdress(String name){
+		User test=new User(name);
+		return users.get(name);
+	}
+	
+	public void add(String newName,String address){
+		User name=new User(newName);
+		users.put(name, address);
+	}
+	
+	
+	public void contains(String key){
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Database database=new Database();
+		database.add("Juan","Bugambilias");
+		//System.out.println(database.getAdress("Juan"));
+		System.out.println(database.getAdress("Juan"));
+		
+		/*User uno = new User("s");
+		User dos = new User("a");
+		System.out.println(uno.equals(dos));*/
+	}
 }
