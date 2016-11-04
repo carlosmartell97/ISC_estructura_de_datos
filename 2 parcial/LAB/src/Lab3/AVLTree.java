17a9f739de8bb1c222b9f1e6921f429376796771
carlosmartell97 <carlosm_97@live.com>
2016-11-04 14:36:58 -0600

    added height() and balance()

--- 2 parcial/LAB/src/Lab3/AVLTree.java
+++ 2 parcial/LAB/src/Lab3/AVLTree.java
@@ -12,6 +12,37 @@ public class AVLTree<Item extends Comparable> {
 		return right;
 	}
 	
+	private int height(AVLNode node){
+		if(node == null)
+			return 0;
+		return node.height;
+	}
+	
+	public AVLNode balance(AVLNode node){
+		if(node == null)
+			return node;
+
+		else if (height(node.left)-height(node.right)>this.ALLOWED_IMBALANCE){
+			//LeftLeft
+			if(height(node.left.left)>=height(node.left.right))
+				node = rightRotate(node);
+			//LeftRight
+			else
+				node = leftRightRotate(node);
+		}
+		//When the right child's height is greater.
+		else if (height(node.right)-height(node.left)>this.ALLOWED_IMBALANCE){
+			//RightRight Case
+			if(height(node.right.right)>=height(node.right.left))
+				node = leftRotate(node);
+			//RightLeft Case
+			else
+				node = rightLeftRotate(node);
+		}
+		node.height = Math.max(height(node.left),height(node.right))+1;
+		return node;
+	}
+	
 	/*public V get(K key){
 		AVLNode temp=this.root;
 		while(temp!=null){
