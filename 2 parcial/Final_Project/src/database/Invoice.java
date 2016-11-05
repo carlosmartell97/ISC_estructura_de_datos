d79038fe144131cc5b554271c99980256e05435a
carlosmartell97 <carlosm_97@live.com>
2016-11-04 23:24:43 -0600

    added constructor method and total()

--- 2 parcial/Final_Project/src/database/Invoice.java
+++ 2 parcial/Final_Project/src/database/Invoice.java
@@ -1,9 +1,20 @@
 package database;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 public class Invoice {
-	protected int total;
+	private int total;
+	protected HashMap<String,Integer> articles=new HashMap();
 	
-	HashMap<String,Integer> articles;
+	public Invoice(String newArticle,Integer newPrice){
+		articles.put(newArticle,newPrice);
+	}
+	
+	public int total(){
+		for(Entry<String, Integer> article: articles.entrySet()){
+			total+=article.getValue();
+		}
+		return total;
+	}
 }
