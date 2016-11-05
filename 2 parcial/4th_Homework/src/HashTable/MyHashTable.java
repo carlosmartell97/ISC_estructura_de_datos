830920753c3d6e65d7ef933a68cdd9664c0e5b0b
carlosmartell97 <carlosm_97@live.com>
2016-11-05 16:57:55 -0600

    basic structure

---
+++ 2 parcial/4th_Homework/src/HashTable/MyHashTable.java
@@ -0,0 +1,13 @@
+package HashTable;
+
+public class MyHashTable<K,V> {
+	private int m,
+				size;
+	private Node<K,V>[] table;
+	private static final int MAX_CAPACITY=101;
+	private static final double DEFAULT_CHARGE=0.75;
+	
+	private static class Node<K,V>{
+		
+	}
+}
