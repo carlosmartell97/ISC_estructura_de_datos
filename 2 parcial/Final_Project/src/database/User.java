b3629f4aa27edb0352519e36aadc6f7f73a7ac9d
carlosmartell97 <carlosm_97@live.com>
2016-11-04 18:09:29 -0600

    added name attribute and constructor method

--- 2 parcial/Final_Project/src/database/User.java
+++ 2 parcial/Final_Project/src/database/User.java
@@ -3,5 +3,10 @@ package database;
 import java.util.HashMap;
 
 public class User {
+	String name;
+	
+	public User(String name){
+		this.name=name;
+	}
 	public HashMap<Invoice,Integer> invoices;
 }
