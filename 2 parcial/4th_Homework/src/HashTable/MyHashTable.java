c7a5d3a0fba87ed9c2476a748cb4ff3756dd19f5
carlosmartell97 <carlosm_97@live.com>
2016-11-05 17:07:01 -0600

    added constructors and hash() with documentation

--- 2 parcial/4th_Homework/src/HashTable/MyHashTable.java
+++ 2 parcial/4th_Homework/src/HashTable/MyHashTable.java
@@ -7,6 +7,27 @@ public class MyHashTable<K,V> {
 	private static final int MAX_CAPACITY=101;
 	private static final double DEFAULT_CHARGE=0.75;
 	
+	public MyHashTable(){
+		this(MAX_CAPACITY);
+	}
+	
+	public MyHashTable(int m){
+		this.m=m;
+		this.size=0;
+		this.table=new Node[m];
+	}
+	
+	/**
+	 * //	convierto el objeto K a su representación
+			numérica. Después, hago un AND a nivel de bits
+			que me quita el signo (valor absoluto)
+	 * @param key
+	 * @return hash code
+	 */
+	public int hash(K key){
+		return key.hashCode()& 0x7FFFFFF5%m;
+	}
+	
 	private static class Node<K,V>{
 		
 	}
