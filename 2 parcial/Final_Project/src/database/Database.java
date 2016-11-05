0d8c071e2e893e59c27cc99582cea7f4d740a24c
carlosmartell97 <carlosm_97@live.com>
2016-11-04 23:25:36 -0600

    running into problems with getInvoiceTotal

--- 2 parcial/Final_Project/src/database/Database.java
+++ 2 parcial/Final_Project/src/database/Database.java
@@ -5,27 +5,39 @@ import java.util.HashMap;
 public class Database {
 	protected static HashMap<String,User> users=new HashMap();
 	
-	public String getAdress(String name){
-		return users.get(name).address;
-	}
-	
-	public void add(String newName,String address){
+	public void addUser(String newName,String address){
 		User user=new User(address);
 		users.put(newName,user);
 	}
 	
+	public void addInvoice(String name,Integer newInvoice,String newArticle,Integer newPrice){
+		Invoice invoice=new Invoice(newArticle,newPrice);
+		users.get(name).invoices.put(newInvoice, invoice);
+	}
 	
-	public boolean contains(String key){
-		return users.containsKey(key);
+	public boolean contains(String name){
+		return users.containsKey(name);
+	}
+	
+	public String getAdress(String name){
+		return users.get(name).address;
+	}
+	
+	public int getInvoiceTotal(String name,Integer invoice){
+		return users.get(name).invoices.get(invoice).total();
 	}
 	
 	public static void main(String[] args) {
 		
 		Database database=new Database();
-		database.add("Juan","Bugambilias");
+		database.addUser("Juan","Bugambilias");
 		
-		System.out.println(database.getAdress("Juan"));
-		System.out.println(database.contains("Juan"));
-		System.out.println(database.contains("John"));
+		System.out.println("getAddress: "+database.getAdress("Juan"));
+		System.out.println("contains: "+database.contains("Juan"));
+		System.out.println("contains: "+database.contains("Pedro"));
+		
+		database.addInvoice("Juan", 0010, "Ketchup", 50);
+		database.addInvoice("Juan", 0010, "Ketchup", 80);
+		System.out.println("total: "+database.getInvoiceTotal("Juan",0010));
 	}
 }
