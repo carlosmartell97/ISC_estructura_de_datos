42bea9e87482965f899b5d96d1941418f75db8df
carlosmartell97 <carlosm_97@live.com>
2016-11-05 19:45:29 -0600

    added visits() and remove()

--- 2 parcial/4th_Homework/src/exercise_4/PatientDataBase.java
+++ 2 parcial/4th_Homework/src/exercise_4/PatientDataBase.java
@@ -25,6 +25,26 @@ public class PatientDataBase extends MyHashTable<String,PatientRecord>{
 		this.size++;
 	}
 	
+	public PatientRecord remove(String name,String date){
+		int pos=this.hash(name);
+		Node<String,PatientRecord> first = this.table[pos];
+		if(first.key.equals(name) && first.value.date.equals(date)){
+			PatientRecord willReturn = first.value;
+			this.table[pos] = first.next;
+			this.size--;
+			return willReturn;
+		}
+		for(Node<String,PatientRecord> x=first; x.next!=null; x=x.next){
+			if(x.next.key.equals(name) && x.next.value.date.equals(date)){
+				PatientRecord willReturn = x.next.value;
+				x.next = x.next.next;
+				this.size--;
+				return willReturn;
+			}
+		}
+		return null;
+	}
+	
 	public String reasonForVisit(String name, String date){
 		PatientRecord record = this.getValue(name,date);
 		if(this.getValue(name,date)==null){
@@ -49,5 +69,16 @@ public class PatientDataBase extends MyHashTable<String,PatientRecord>{
 			}
 		}
 		return null;
+	}
+	
+	public ArrayLinearList<String> visits(String name){
+		int pos = this.hash(name);
+		ArrayLinearList<String> visits = new ArrayLinearList<String>();
+		for(Node<String,PatientRecord> x=this.table[pos]; x!=null; x=x.next){
+			if(x.key.equals(name)){
+				visits.add(visits.size(),x.value.date);
+			}
+		}
+		return visits;
 	}
 }
