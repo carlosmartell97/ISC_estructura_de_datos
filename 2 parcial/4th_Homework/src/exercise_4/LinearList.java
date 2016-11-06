004b61688abc6607ce12c317fd2d7d2bb7050df5
carlosmartell97 <carlosm_97@live.com>
2016-11-05 19:31:55 -0600

    this interface is implemented by ArrayLinearList.java

---
+++ 2 parcial/4th_Homework/src/exercise_4/LinearList.java
@@ -0,0 +1,12 @@
+package exercise_4;
+
+public interface LinearList<Item> {
+	public boolean isEmpty();
+	public int size();
+	public Item get(int index);
+	public int indexOf(Item item);
+	public Item remove(int index);
+	public void add(int index,Item item);
+	public String toString();
+	
+}
