c31bf3b5b06bd535d5432b2f51d5a274d6db4d7d
carlosmartell97 <carlosm_97@live.com>
2016-11-05 18:47:33 -0600

    added constructor method and toString()

--- 2 parcial/4th_Homework/src/exercise_4/PatientRecord.java
+++ 2 parcial/4th_Homework/src/exercise_4/PatientRecord.java
@@ -5,4 +5,14 @@ public class PatientRecord {
 	public String date;
 	public String reasonForVisit;
 	public String treatment;
+	
+	public PatientRecord(String name,String date, String reason, String treatment){
+		this.name = name;
+		this.date = date;
+		this.reasonForVisit = reason;
+		this.treatment = treatment;
+	}
+	public String toString(){
+		return "-name: "+this.name+" -date: "+this.date+" -reasonForVisit: "+this.reasonForVisit+" -treatment: "+this.treatment;
+	}
 }
