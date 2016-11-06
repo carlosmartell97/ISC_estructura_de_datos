e54c73452560a8160636f0bd271dfec2413185a8
carlosmartell97 <carlosm_97@live.com>
2016-11-05 19:04:26 -0600

    added constructor method and add()

--- 2 parcial/4th_Homework/src/exercise_4/PatientDataBase.java
+++ 2 parcial/4th_Homework/src/exercise_4/PatientDataBase.java
@@ -2,4 +2,22 @@ package exercise_4;
 
 public class PatientDataBase extends MyHashTable<String,PatientRecord>{
 	
+	public PatientDataBase(){
+		super();
+	}
+	
+	public void add(String key, PatientRecord record, String date){
+		if(this.size >= this.m-1 || (this.size+0.0)/(this.m) >= DEFAULT_CHARGE)
+			this.rehash();
+		int pos = this.hash(key);
+		for(Node<String,PatientRecord> x=this.table[pos]; x!=null; x=x.next){
+			if(x.key.equals(key) && x.value.date.equals(date)){
+				x.value = record;
+				return;
+			}
+		}
+		Node<String,PatientRecord> newRecord = new Node<String,PatientRecord>(key,record,this.table[pos]);
+		this.table[pos] = newRecord;
+		this.size++;
+	}
 }
