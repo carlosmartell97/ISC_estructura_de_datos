02724e9ee9dbd19d9b8aa64de0971578cb6faffc
carlosmartell97 <carlosm_97@live.com>
2016-11-11 16:32:38 -0600

    basic structure

---
+++ 3 parcial/LAB/src/Lab1/DisjointSet.java
@@ -0,0 +1,8 @@
+package Lab1;
+
+public class DisjointSet {
+	int[] p;	// para los padres
+	int[] rank;	// para los rangos de cada nodo
+	int[] size;	// para los tamaños de cada nodo
+	int numSets;
+}
