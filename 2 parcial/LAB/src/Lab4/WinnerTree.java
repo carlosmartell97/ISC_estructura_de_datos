2824c67d7f7f8c30f0e46005c3dfdac70699a3a6
carlosmartell97 <carlosm_97@live.com>
2016-11-18 15:37:34 -0600

    corrected mistakes in arithmetic within methods for order of operations, along with other little changes. Testing in main now shows it's working!

--- 2 parcial/LAB/src/Lab4/WinnerTree.java
+++ 2 parcial/LAB/src/Lab4/WinnerTree.java
@@ -5,18 +5,23 @@ package Lab4;
  * @author dell
  *
  */
-public class WinnerTree<Item extends Comparable<Item>> implements TournamentTree<Item>{
-	private Item[] players;
+public class WinnerTree implements TournamentTree<Integer>{
+	private Integer[] players;
 	private int[] tree;
 	private int lowExt;
 	private int offset;
 	
-	public Item getWinner(){
-		return (this.tree==null)?
-				players[tree[0]] : null;
+	public Integer getWinner(){
+		return (this.tree!=null)?
+				players[tree[1]] : null;
 	}
 	
-	public void initialize(Item[] a){
+	public int winnerIndex(){
+		return (tree != null)?
+				tree[1]:null;
+	}
+	
+	public void initialize(Integer[] a){
 		int n=a.length-1;
 		if(n<2){
 			throw new IllegalArgumentException("there should be at least 2 players");
@@ -28,41 +33,39 @@ public class WinnerTree<Item extends Comparable<Item>> implements TournamentTree
 		int i,s;
 		for(s=1;s*2<=n-1;s+=s);
 		lowExt=2*(n-s);
-		offset=(2*s)-1;
+		offset=2*s-1;
 		//	we use play() so we can form the tree from the external
 		//	nodes upwards.
-		for(i=2;i<=lowExt;i+=2){
+		for(i=2;i<=lowExt;i+=2)
 			play((i+offset)/2,i-1,i);
-		}
 		if(n%2==1){	//	este caso es cuando voy a tener una match con solo un jugador
-			play(n/2,tree[n-1],lowExt++);
+			play(n/2,tree[n-1],lowExt+1);
 			i=lowExt+3;	//	tengo que avanar 3 lugares
 		}
-		else{	//	lal cantidad de jugadores es par, y sí podré 2 lugares
+		else	//	lal cantidad de jugadores es par, y sí podré 2 lugares
 			i=lowExt+2;
-		}
-		for(;i<=n;i+=2){ //	vas jugando de 2 en 2
+		
+		for(;i<=n;i+=2) //	vas jugando de 2 en 2
 			play((i-lowExt+n-1)/2,i-1,i);
-		}
 	}
 	
 	public void play(int parent, int left, int right){
 		tree[parent] = (players[left].compareTo(players[right])<0)?left:right; //	que el padre sea el ganador, el más chico
-		while(parent%2==1){ //	seguir jugando
-			tree[parent/2] = (players[tree[parent-1]].compareTo(players[tree[parent]])<0)?parent-1:parent;
+		while(parent%2==1 && parent>1){ //	seguir jugando
+			tree[parent/2] = (players[tree[parent-1]].compareTo(players[tree[parent]])<0)?tree[parent-1]:tree[parent];
+			parent/=2;
 		}
 	}
 	
 	public void replay(int thePlayer){
 		//	falta documentar qué pasa en cada paso
 		int n=players.length-1;
-		if(thePlayer<=0 || thePlayer>n){
+		if(thePlayer<=0 || thePlayer>n)
 			throw new IndexOutOfBoundsException(thePlayer+": invalid player");
-		}
 		int matchNode, leftChild, rightChild;
 		if(thePlayer<=lowExt){ //	potencia de 2
 			matchNode=(offset+thePlayer)/2;	// lugar donde irá ganador de leftChild y rightChild
-			leftChild=2*(matchNode-offset);
+			leftChild=2*matchNode-offset;
 			rightChild=leftChild+1;
 		}
 		else{ //	no es potencia de 2
@@ -89,14 +92,14 @@ public class WinnerTree<Item extends Comparable<Item>> implements TournamentTree
 		}
 	}
 	
-	public void change(int thePlayer,Item value){
+	public void change(int thePlayer,Integer value){
 		this.players[thePlayer]=value;	// asignar valor
-		replay(thePlayer);	// acomodar todo
+		//replay(thePlayer);	// acomodar todo
 	}
 	
-	public Item[] sort(Item[] a){
+	public Integer[] sort(Integer[] a){
 		this.initialize(a);
-		Item[] order=(Item[]) new Object[players.length];
+		Integer[] order=(Integer[]) new Integer[players.length];
 		order[0]=null;
 		for(int i=1;i<players.length;i++){
 			order[i]=this.players[tree[i]];
@@ -115,17 +118,23 @@ public class WinnerTree<Item extends Comparable<Item>> implements TournamentTree
 	}
 	
 	public static void main(String[] args){
-		WinnerTree<Integer> tree = new WinnerTree();
-		Integer[] a={0,4,6,9,25,10,7,3,2};
+		WinnerTree tree = new WinnerTree();
+		Integer[] a = {0,4,9,3,5,1,6};
 		tree.initialize(a);
-		System.out.println(tree);
-		tree.replay(2);
-		System.out.println(tree);
-		/*int[]b=tree.sort(a);
 		
-		for(int i=0;i<b.length;i++){
-			System.out.println(b[i]);
-		}*/
+		System.out.println("initialized: "+tree);
+		System.out.println("wins: "+tree.getWinner());
+		System.out.println();
+		
+		System.out.println("MATCHES:");
+		Integer[] b = new Integer[a.length];
+		for(int i = 1; i < a.length; i++){
+			b[i] = tree.getWinner();
+			System.out.println("Winner-> "+tree.getWinner());
+			tree.change(tree.winnerIndex(),0x7FFFFFFF); //
+			tree.replay(tree.winnerIndex());
+		}
+		//System.out.println(Arrays.toString(b));
 	}
 }
 
