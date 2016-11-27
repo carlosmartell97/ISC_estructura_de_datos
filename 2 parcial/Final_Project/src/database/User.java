693692de024899313466316ee8f003c210df8965
chuyalvarez <jesusaagomez@hotmail.com>
2016-11-27 17:02:07 -0600

    added attribute userExpense

--- 2 parcial/Final_Project/src/database/User.java
+++ 2 parcial/Final_Project/src/database/User.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class User {
 	protected String address;
 	protected HashMap<Integer,Invoice> invoices=new HashMap();
+	protected int userExpense;
 	
 	public User(String address){
 		this.address=address;
