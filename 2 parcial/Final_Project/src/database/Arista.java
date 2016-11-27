c87bf28ba65e2989a02bdb96e058b655918393c4
chuyalvarez <jesusaagomez@hotmail.com>
2016-11-27 14:31:44 -0600

    Arista.java

---
+++ 2 parcial/Final_Project/src/database/Arista.java
@@ -0,0 +1,19 @@
+package database;
+
+public class Arista {
+	private Vertice destino;
+	private double costo;
+	
+	public Vertice destino(){
+		return this.destino;
+	}
+	
+	public double costo(){
+		return this.costo;
+	}
+	
+	public Arista(Vertice destino,double costo){
+		this.destino=destino;
+		this.costo=costo;
+	}
+}
