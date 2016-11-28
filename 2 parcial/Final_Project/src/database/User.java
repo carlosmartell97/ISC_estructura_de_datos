19f158a0edff7e32b9be60283545a6c6c132747b
carlosmartell97 <carlosm_97@live.com>
2016-11-27 19:23:43 -0600

    removed attribute userExpense

--- 2 parcial/Final_Project/src/database/User.java
+++ 2 parcial/Final_Project/src/database/User.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 public class User {
 	protected String address;
 	protected HashMap<Integer,Invoice> invoices=new HashMap();
-	protected int userExpense;
 	
 	public User(String address){
 		this.address=address;
