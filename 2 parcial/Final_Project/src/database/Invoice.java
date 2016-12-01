dfddca07c4ca0dbaacaf8e3e660c42c18d6625d0
chuyalvarez <jesusaagomez@hotmail.com>
2016-12-01 10:47:23 -0600

    documentation

--- 2 parcial/Final_Project/src/database/Invoice.java
+++ 2 parcial/Final_Project/src/database/Invoice.java
@@ -6,25 +6,26 @@ import java.util.Map.Entry;
 public class Invoice {
 	private int total;
 	protected int invoiceRevenue;
-	protected HashMap<Integer,AVLTree> articles=new HashMap();
+	protected HashMap<Integer,AVLTree> articles=new HashMap(); //Hash map that stores multiple articles as an avl tree for rapid indexation when removing
 	
+	//Class constructor in case this is a new invoice, calls addArticle().
 	public Invoice(int newArticle,Integer newPrice){
 		this.addArticle(newArticle,newPrice);
 	}
 	
 	public void addArticle(int productCode,Integer newPrice){
-		total+=newPrice;
+		total+=newPrice; //keeps track of invoice total expense
 		if(articles.containsKey(productCode)){
-			articles.get(productCode).insert(newPrice);
+			articles.get(productCode).insert(newPrice); //add an item when it already exists
 		}
 		else{
-			AVLTree tree=new AVLTree();
-			tree.insert(newPrice);
+			AVLTree tree=new AVLTree(); //creation of item
+			tree.insert(newPrice);		 	
 			articles.put(productCode,tree);
 		}
 	}
 	
 	public int total(){
-		return total;
+		return total; 
 	}
 }
